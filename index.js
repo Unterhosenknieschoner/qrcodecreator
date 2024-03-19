58d2d14bee0b760c7e43d25d4bc258305acedea6
Unterhosenknieschoner <michi@og-hosting.com>
2024-03-19 16:11:14 +0100

    Creates a .SVG file in the projects directory

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-//Made by DJ Tingombrax
+// Created by DJ Tingombrax
+const qrcode = require('qrcode');
 
 
-// Command line input shizz
+//All the jazzy input shizz
 const readline = require('readline');
 
 function getInput(prompt) {
-    const rl = readline. createInterface({
+    const rl = readline.createInterface({
         input: process.stdin,
-        output: process. stdout
+        output: process.stdout
 
     });
 
     return new Promise((resolve, reject) => {
-        rl.question (prompt, (input) => {
+        rl.question(prompt, (input) => {
             rl.close();
             resolve(input);
-
         });
+    });
+}
 
-// user input:
+// User input:
         async function main() {
             try {
-                const name = await getInput("Enter a link.");
-                console. log('Link copied!, ${name}!');
+                const link = await getInput("Enter a link. ");
+                console.log(`Processing..., ${link}!`);
+
+
+                qrcode.toFile('qrcode.svg', link, {
+                    errorCorrectionLevel: 'L',
+                    margin: 4,
+                    scale: 10,
+                }, (err) => {
+                    if (err) throw err;
+                    console.log('QR-Code generated successfully!');
+                });
+
+//Possibly console output
+
+                qrcode.toString(`${link}`,{type:'terminal'}, function (err, url) {
+                    console.log(url)
+                })
+
             } catch (error) {
-                console.error("An error occurred:", error)
+                console.error("An error occurred:", error);
+            }
+        }
 
-//Create QRcode
 
+
+
+
+                main();
